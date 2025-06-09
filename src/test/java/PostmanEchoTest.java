6caa4e2dcf5119ce717545e6ff22d12a29d9f9b1
Svyatoslav Starkov <svyatoslav.starkov@yandex.ru>
2025-06-09 18:06:07 +0400

    Сhange PostmanEchoTest

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -18,6 +18,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo(Body));
+                .body("data", equalTo(requestBody));
     }
 }
