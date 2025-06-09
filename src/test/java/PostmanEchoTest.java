be00de3a2560f180b609c45eeac47c46e2b1d56f
Svyatoslav Starkov <svyatoslav.starkov@yandex.ru>
2025-06-09 18:02:08 +0400

    Add PostmanEchoTest

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldSendPostRequestAndValidateResponse() {
+        String baseUri = "https://postman-echo.com";
+        String requestBody = "some data";
+
+        RestAssured.given()
+                .baseUri(baseUri)
+                .contentType("text/plain; charset=UTF-8")
+                .body(requestBody)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(requestBody));
+    }
+}
